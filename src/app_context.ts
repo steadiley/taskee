e0eca5566be789143b1db1b0b2b280eb8c468ddd
Otsuki Hitoshi <hitochan777@gmail.com>
2020-04-29 17:19:14 +0900

    Use Inmemory repository for e2e test for now

--- src/app_context.ts
+++ src/app_context.ts
@@ -2,13 +2,21 @@ import { Container } from "inversify";
 import getDecorators from "inversify-inject-decorators";
 
 import { TaskUsecase, AppTaskUsecase } from "@/app";
+import { InmemoryTaskRepository } from "@/infra/repository/inmemory/task";
 import { FirestoreTaskRepository } from "@/infra/repository/firestore/task";
 import { TaskRepository } from "./domain/repository";
 
 const container = new Container();
-container
-  .bind<TaskRepository>("TaskRepository")
-  .toConstantValue(new FirestoreTaskRepository());
+
+if (process.env.NODE_ENV === "test") {
+  container
+    .bind<TaskRepository>("TaskRepository")
+    .toConstantValue(new InmemoryTaskRepository());
+} else {
+  container
+    .bind<TaskRepository>("TaskRepository")
+    .toConstantValue(new FirestoreTaskRepository());
+}
 
 container
   .bind<TaskUsecase>("TaskUsecase")
