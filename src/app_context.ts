c00d30bbbed9b6e81205be55882ee1b5aa241fbb
Otsuki Hitoshi <hitochan777@gmail.com>
2020-04-29 15:31:58 +0900

    Use firestore repository by default

--- src/app_context.ts
+++ src/app_context.ts
@@ -2,13 +2,13 @@ import { Container } from "inversify";
 import getDecorators from "inversify-inject-decorators";
 
 import { TaskUsecase, AppTaskUsecase } from "@/app";
-import { InmemoryTaskRepository } from "@/infra/repository/inmemory/task";
+import { FirestoreTaskRepository } from "@/infra/repository/firestore/task";
 import { TaskRepository } from "./domain/repository";
 
 const container = new Container();
 container
   .bind<TaskRepository>("TaskRepository")
-  .toConstantValue(new InmemoryTaskRepository());
+  .toConstantValue(new FirestoreTaskRepository());
 
 container
   .bind<TaskUsecase>("TaskUsecase")
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,15 @@
-import firebase from "firebase";
+import * as firebase from "firebase/app";
 import "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+};
+
 // TODO: to be fixed via environment variables
-firebase.initializeApp({
-  apiKey: "### FIREBASE API KEY ###",
-  authDomain: "### FIREBASE AUTH DOMAIN ###",
-  projectId: "### CLOUD FIRESTORE PROJECT ID ###",
-});
+firebase.initializeApp(firebaseConfig);
