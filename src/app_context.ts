7c9e3bf08a5ead248d6e8f6fe43372fb451ee252
Otsuki Hitoshi <hitochan777@gmail.com>
2020-05-03 04:34:52 +0900

    Do not import unnecessary modules

--- src/app_context.ts
+++ src/app_context.ts
@@ -3,19 +3,16 @@ import { Container } from "inversify";
 import getDecorators from "inversify-inject-decorators";
 
 import { TaskUsecase, AppTaskUsecase } from "@/app";
-import {
-  InmemoryTaskRepository,
-  InmemoryTaskEventRepository,
-} from "@/infra/repository/inmemory";
-import {
-  FirestoreTaskRepository,
-  FirestoreTaskEventRepository,
-} from "@/infra/repository/firestore";
+
 import { TaskRepository, TaskEventRepository } from "./domain/repository";
 
 const container = new Container();
 
 if (process.env.NODE_ENV === "test") {
+  const {
+    InmemoryTaskRepository,
+    InmemoryTaskEventRepository,
+  } = require("@/infra/repository/inmemory");
   container
     .bind<TaskRepository>("TaskRepository")
     .toConstantValue(new InmemoryTaskRepository());
@@ -23,6 +20,10 @@ if (process.env.NODE_ENV === "test") {
     .bind<TaskEventRepository>("TaskEventRepository")
     .toConstantValue(new InmemoryTaskEventRepository());
 } else {
+  const {
+    FirestoreTaskRepository,
+    FirestoreTaskEventRepository,
+  } = require("@/infra/repository/firestore");
   container
     .bind<TaskRepository>("TaskRepository")
     .toConstantValue(new FirestoreTaskRepository());
