01e34751109a1901e7d8e108dfe33cf259a99846
nakagawa <237s10mimi@gmail.com>
2020-05-03 20:12:47 +0900

    add-Container

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view />
+    <UiContainer mode="dark">
+      <div id="nav">
+        <router-link to="/">Home</router-link>
+      </div>
+      <router-view />
+    </UiContainer>
   </div>
 </template>
 
@@ -12,8 +14,13 @@ import { defineComponent, onMounted } from "@vue/composition-api";
 
 import { provideStore, useTaskStore } from "./composables/use_store";
 
+import UiContainer from "@/components/ui/Container.vue";
+
 const App = defineComponent({
   name: "App",
+  components: {
+    UiContainer,
+  },
   setup(_, { root: { $store } }) {
     provideStore($store);
 
---
+++ src/components/ui/Container.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container" :class="colorMode">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "@vue/composition-api";
+
+interface Props {
+  mode: string;
+}
+
+const Container = defineComponent({
+  name: "Container",
+  props: {
+    mode: { type: String, default: "" },
+  },
+  setup(props: Props) {
+    const colorMode =
+      props.mode === "dark" ? computed(() => `${props.mode}`) : "";
+    return {
+      colorMode,
+    };
+  },
+});
+export default Container;
+</script>
+
+<style scoped lang="scss">
+.container {
+  height: 100vh;
+  background-color: $background-color;
+  &.dark {
+    background-color: $darkmode-background-color;
+    color: $darkmode-white-op7;
+  }
+}
+</style>
