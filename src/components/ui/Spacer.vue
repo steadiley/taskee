687eaabe433a42f316ed98b207ddf4bf01bc47cb
nakagawa <237s10mimi@gmail.com>
2020-05-04 19:42:36 +0900

    add-spacer

---
+++ src/components/ui/Spacer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="spacer"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+const Spacer = defineComponent({
+  name: "Spacer",
+  components: {},
+  props: {},
+  setup: () => {
+    return {};
+  },
+});
+export default Spacer;
+</script>
+
+<style scoped lang="scss">
+.spacer {
+  flex-grow: 1;
+}
+</style>
