1f4d8619c30cde0941fd2103c52203df2d8d7c8e
nakagawa <237s10mimi@gmail.com>
2020-05-26 08:39:00 +0900

    add-checkbox-component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "prettier/prettier": "error",
     eqeqeq: "error",
-    "@typescript-eslint/no-var-requires": "off"
+    "@typescript-eslint/no-var-requires": "off",
   },
   overrides: [
     {
       files: [
         "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 };
--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,5 +1,16 @@
 <template>
   <UiCard>
+    <UiCheckbox
+      v-for="(option, index) in checkOptions"
+      v-model="myCheckBoxModel"
+      :key="index"
+      :index="index"
+      :input-value="task.id"
+      :label="option.label"
+      :labelPosition="option.labelPosition"
+      :color="option.color"
+    />
+    <div v-for="item in myCheckBoxModel" :key="item">{{ item }}</div>
     <div>{{ task.title }}</div>
     <div>Total: {{ totalTimeSpent }} minutes</div>
     <button @click="toggleTimer(isRunning)">
@@ -9,10 +20,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "@vue/composition-api";
+import { defineComponent, computed, reactive } from "@vue/composition-api";
 import { Task } from "../domain/entity";
 import { useTaskStore } from "../composables/use_store";
 import UiCard from "@/components/ui/Card.vue";
+import UiCheckbox from "@/components/ui/Checkbox.vue";
 
 interface Props {
   task: Task;
@@ -21,6 +33,7 @@ interface Props {
 
 export const useTimer = (taskId: string) => {
   const taskStore = useTaskStore();
+  console.log(taskStore);
   const toggleTimer = async (isRunning: boolean) => {
     if (isRunning) {
       await taskStore.stopRunningTask();
@@ -37,12 +50,33 @@ const TaskCard = defineComponent({
   name: "TaskCard",
   components: {
     UiCard,
+    UiCheckbox,
   },
   props: {
     task: Object,
     runningTask: Object,
   },
   setup(props: Props) {
+    const checkOptions = [
+      {
+        label: "Option 1",
+        value: "value of option 1",
+        labelPosition: "left",
+        color: "red",
+      },
+      {
+        label: "Option 2",
+        value: "value of option 2",
+      },
+      {
+        label: "Option 3",
+        value: "value of option 3",
+      },
+      {
+        value: "value of option 4",
+      },
+    ];
+    const myCheckBoxModel = reactive([]);
     const { toggleTimer } = useTimer(props.task.id);
     const isRunning = computed(() => {
       return props.runningTask && props.task.id === props.runningTask.task.id;
@@ -56,6 +90,8 @@ const TaskCard = defineComponent({
       toggleTimer,
       isRunning,
       totalTimeSpent,
+      checkOptions,
+      myCheckBoxModel,
     };
   },
 });
